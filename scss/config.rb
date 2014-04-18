a8f53bdebbdc98446d88b9e244a472f96084ad55
Egor Koshelko <egor.koshelko@gmail.com>
2014-04-18 17:22:02 +0700

    Initial commit

---
+++ scss/config.rb
@@ -0,0 +1,7 @@
+sass_path = File.dirname(__FILE__)
+css_path = "../css"
+images_path = "../img"
+generated_images_dir = "../static/public/img/sprites-build"
+http_generated_images_path = "../img/sprites-build"
+
+output_style = :compressed
